675b6afba434875c32723486eff0cf942289e63a
xmanhit <nxmanh102@gmail.com>
2023-09-26 14:03:31 +0700

    update not found

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -5,19 +5,23 @@ import NotFoundIcon from '../../assets/notfound.svg'
 const NotFound: React.FC = () => {
   return (
     <main>
-      <div className={styles.container}>
-        <NotFoundIcon />
-        <div className={styles.content}>
-          <h1>404</h1>
-          <h2>UH OH! You're lost.</h2>
-          <p>
-            The page you are looking for does not exist. How you got here is a mystery. But you can click the button
-            below to go back to the homepage.
-          </p>
+      <div className={styles.notfound}>
+        <div className={styles.image}>
+          <NotFoundIcon />
         </div>
-        <Link to={'/'} className={styles.link}>
-          HOME
-        </Link>
+        <div className={styles.wrapper}>
+          <div className={styles.content}>
+            <h1>404</h1>
+            <h2>UH OH! You're lost.</h2>
+            <p>
+              The page you are looking for does not exist. How you got here is a mystery. But you can click the button
+              below to go back to the homepage.
+            </p>
+          </div>
+          <Link to={'/'} className={styles.link}>
+            HOME
+          </Link>
+        </div>
       </div>
     </main>
   )
