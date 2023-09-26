641dd7d410d75eae4104760c017d1f72a8ac93ef
xmanhit <nxmanh102@gmail.com>
2023-09-26 13:48:54 +0700

    update page not found

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,5 +1,26 @@
+import { Link } from 'react-router-dom'
+import styles from '../../styles/NotFound.module.css'
+import NotFoundIcon from '../../assets/notfound.svg'
+
 const NotFound: React.FC = () => {
-  return <div>NotFound</div>
+  return (
+    <main>
+      <div className={styles.container}>
+        <NotFoundIcon />
+        <div className={styles.content}>
+          <h1>404</h1>
+          <h2>UH OH! You're lost.</h2>
+          <p>
+            The page you are looking for does not exist. How you got here is a mystery. But you can click the button
+            below to go back to the homepage.
+          </p>
+        </div>
+        <Link to={'/'} className={styles.link}>
+          HOME
+        </Link>
+      </div>
+    </main>
+  )
 }
 
 export default NotFound
