d781ad88216337136781a3b49b04b7a1aab8f01a
xmanhit <nxmanh102@gmail.com>
2023-09-28 16:50:23 +0700

    update title page

--- src/router.tsx
+++ src/router.tsx
@@ -13,28 +13,28 @@ const router = createBrowserRouter([
         index: true,
         lazy: async () => {
           const { homeLoader, default: Home } = await import('./pages/Home')
-          return { loader: homeLoader, Component: Home }
+          return { loader: homeLoader, Component: Home, title: 'Home' }
         },
       },
       {
         path: 'tags/:tag',
         lazy: async () => {
           const Tags = await import('./pages/Tags')
-          return { Component: Tags.default }
+          return { Component: Tags.default, title: 'Tags' }
         },
       },
       {
         path: 'following',
         lazy: async () => {
           const { homeLoader, default: Home } = await import('./pages/Home')
-          return { loader: homeLoader, Component: Home }
+          return { loader: homeLoader, Component: Home, title: 'Following' }
         },
       },
       {
         path: ':author/:slug',
         lazy: async () => {
           const ArticleDetails = await import('./pages/ArticleDetails')
-          return { Component: ArticleDetails.default }
+          return { Component: ArticleDetails.default, title: 'Article' }
         },
       },
       {
@@ -44,14 +44,14 @@ const router = createBrowserRouter([
             index: true,
             lazy: async () => {
               const UserDetails = await import('./pages/UserDetails')
-              return { Component: UserDetails.default }
+              return { Component: UserDetails.default, title: 'User' }
             },
           },
           {
             path: 'favorites',
             lazy: async () => {
               const UserFavorites = await import('./pages/UserFavorites')
-              return { Component: UserFavorites.default }
+              return { Component: UserFavorites.default, title: 'Favorites' }
             },
           },
         ],
@@ -70,21 +70,21 @@ const router = createBrowserRouter([
             index: true,
             lazy: async () => {
               const UserDetails = await import('./pages/UserDetails')
-              return { Component: UserDetails.default }
+              return { Component: UserDetails.default, title: 'Me' }
             },
           },
           {
             path: 'favorites',
             lazy: async () => {
               const UserFavorites = await import('./pages/UserFavorites')
-              return { Component: UserFavorites.default }
+              return { Component: UserFavorites.default, title: 'Favorites' }
             },
           },
           {
             path: 'settings',
             lazy: async () => {
               const UserSetting = await import('./pages/UserSettings')
-              return { Component: UserSetting.default }
+              return { Component: UserSetting.default, title: 'Settings' }
             },
           },
           {
@@ -99,7 +99,7 @@ const router = createBrowserRouter([
             path: '*',
             lazy: async () => {
               const NotFound = await import('./pages/NotFound')
-              return { Component: NotFound.default }
+              return { Component: NotFound.default, title: 'Not Found' }
             },
           },
         ],
@@ -108,7 +108,7 @@ const router = createBrowserRouter([
         path: '*',
         lazy: async () => {
           const NotFound = await import('./pages/NotFound')
-          return { Component: NotFound.default }
+          return { Component: NotFound.default, title: 'Not Found' }
         },
       },
     ],
@@ -118,35 +118,35 @@ const router = createBrowserRouter([
     path: '/login',
     lazy: async () => {
       const { loginLoader, default: Login } = await import('./pages/Login')
-      return { loader: loginLoader, Component: Login }
+      return { loader: loginLoader, Component: Login, title: 'Login' }
     },
   },
   {
     path: '/register',
     lazy: async () => {
       const { registerLoader, default: Register } = await import('./pages/Register')
-      return { loader: registerLoader, Component: Register }
+      return { loader: registerLoader, Component: Register, title: 'Register' }
     },
   },
   {
     path: '/new',
     lazy: async () => {
       const { formArticleLoader, default: FormArticle } = await import('./pages/FormArticle')
-      return { loader: formArticleLoader, Component: FormArticle }
+      return { loader: formArticleLoader, Component: FormArticle, title: 'New Article' }
     },
   },
   {
     path: '/:slug/edit',
     lazy: async () => {
       const { formArticleLoader, default: FormArticle } = await import('./pages/FormArticle')
-      return { loader: formArticleLoader, Component: FormArticle }
+      return { loader: formArticleLoader, Component: FormArticle, title: 'Edit Article' }
     },
   },
   {
     path: '*',
     lazy: async () => {
       const NotFound = await import('./pages/NotFound')
-      return { Component: NotFound.default }
+      return { Component: NotFound.default, title: 'Not Found' }
     },
   },
 ])
